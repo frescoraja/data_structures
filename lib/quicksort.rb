a779792e26b1502617489e6a0613b031d356ce80
David James Carter <fresco.raja@gmail.com>
2015-08-02 19:39:01 -0700

    quicksort

--- lib/quicksort.rb
+++ lib/quicksort.rb
@@ -1,24 +1,27 @@
-# def partition(arr)
-#   p_idx = 0
-#   pivot = arr[p_idx]
-#   (1...arr.length).each do |idx|
-#     if arr[idx] < pivot
-#       cur_el = arr[idx]
-#       arr[p_idx + 1], arr[idx] = arr[idx], arr[p_idx + 1]
-#       arr[p_idx] = cur_el
-#       p_idx += 1
-#     end
-#   end
-#   arr
-# end
-def partition (arr)
-	p_idx = 0
-	(1...arr.length).each do |idx|
-		if arr[idx] < arr[p_idx]
-			 arr[p_idx + 1], arr[idx] = arr[idx], arr[p_idx + 1]
-			 arr[p_idx], arr[p_idx + 1] = arr[p_idx + 1], arr[p_idx]
-		 	 p_idx += 1
-		end
-   end
-	arr
+def quicksort(arr, start = 0, len = arr.length - start)
+  return arr[start..len + start] if len < 2
+  raise "start index must be in array" unless start.between?(0, (arr.length - 1))
+  raise "range exceeds length of array!" if start + len > arr.length
+
+  pivot_idx = partition!(arr, start, len)
+  quicksort(arr, start, pivot_idx - start)
+  quicksort(arr, pivot_idx + 1, len - (pivot_idx + 1))
+
+  return arr
+end
+
+def partition!(arr, start, len)
+  end_idx = start + len - 1
+  p_idx = start
+  start_idx = start + 1
+  while start_idx <= end_idx
+	  if arr[start_idx] < arr[p_idx]
+      arr[p_idx + 1], arr[start_idx] = arr[start_idx], arr[p_idx + 1]
+      arr[p_idx], arr[p_idx + 1] = arr[p_idx + 1], arr[p_idx]
+      p_idx += 1
+     end
+    start_idx += 1
+  end
+
+  return p_idx
 end
